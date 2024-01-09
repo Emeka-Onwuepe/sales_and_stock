e3371e9fda2bce833b14ad0f0f4c456010c5e330
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2024-01-09 23:01:28 +0100

    added sales app

--- Sales/models.py
+++ Sales/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.forms import BooleanField
+from Branch.models import Branch
+from Product.models import Product, Size, Suit, Top
+from User.models import Customer
 
 # Create your models here.
+
+class Items(models.Model):
+    """Model definition for Items."""
+    
+    SUIT = 'suit'
+    FOOT_WEARS = 'foot_wears'
+    TOP = 'top'
+    DOWN  = 'down'
+    PRODUCT = 'product'
+
+    P_GROUP = {SUIT:'suit',
+               FOOT_WEARS:'foot_wears',
+               TOP :'top',
+            #    DOWN :'down',
+               PRODUCT :'product',
+                } 
+    
+    p_group = models.CharField(
+        max_length=10,
+        choices=P_GROUP,
+        default=PRODUCT,
+    )
+    
+    product = models.ForeignKey(Product, verbose_name="product", 
+                    related_name="selected_product", on_delete=models.CASCADE,null=True, blank=True)
+    suit = models.ForeignKey(Suit, verbose_name="suit", 
+                    related_name="selected_suit", on_delete=models.CASCADE,null=True, blank=True)
+    top = models.ForeignKey(Top, verbose_name="top", 
+                    related_name="selected_top", on_delete=models.CASCADE,null=True, blank=True)
+    product_type = models.CharField('product_type', max_length = 200)
+    size_instance = models.ForeignKey(Size, verbose_name="size_instance", on_delete=models.CASCADE,
+                        related_name="items_size_instance",null=True, blank=True)
+    qty = models.IntegerField("qty")
+    unit_price = models.DecimalField("unit_price", max_digits=10, decimal_places=2)
+    total_price = models.DecimalField("total_price", max_digits=10, decimal_places=2)
+    mini_price = models.DecimalField("mini_price", max_digits=10, decimal_places=2)
+    expected_price = models.DecimalField("expected_price", max_digits=10, decimal_places=2)
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Items."""
+
+        verbose_name = 'Items'
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        """Unicode representation of Items."""
+        return f"{self.product_type} - {str(self.total_price)}"
+
+
+class Sales(models.Model):
+    """Model definition for Sales."""
+    CHANNEL_CHOICES = (
+        ('web', 'web'),
+        ('store', 'store'),
+    )
+    PAYMENT_CHOICES = (
+        ('transfer', 'transfer'),
+        ('cash', 'cash'),
+        ('credit', 'credit'),
+        ('on_web', 'on_web'),
+    )
+
+    # TODO: Define fields here
+    branch = models.ForeignKey(Branch,related_name="sales_branch", verbose_name="branch", on_delete=models.CASCADE)
+    total_amount = models.DecimalField("total_amount", max_digits=10, decimal_places=2)
+    expected_amount = models.DecimalField("expected_amount", max_digits=10, decimal_places=2)
+    logistics = models.DecimalField("logistics", max_digits=10, decimal_places=2,default=0.00)
+    destination = models.CharField("destination", max_length=256,default="not set",blank=True)
+    remark = models.CharField("remark", max_length=200)
+    channel = models.CharField("channel", max_length=5, choices=CHANNEL_CHOICES, default='store')
+    payment_method = models.CharField("payment_method", max_length=8, choices=PAYMENT_CHOICES, default='cash')
+    date = models.DateField("date", auto_now=False, auto_now_add=True)
+    customer = models.ForeignKey(Customer, verbose_name="customer",related_name="sales_customer",on_delete=models.CASCADE)
+    items = models.ManyToManyField(Items, verbose_name="items",related_name="items")
+    purchase_id = models.CharField("purchase_id", max_length=150)
+    paid = models.BooleanField("paid",default=True)
+    
+    class Meta:
+        """Meta definition for Sales."""
+
+        verbose_name = 'Sales'
+        verbose_name_plural = 'Sales'
+        ordering = ["-date"]
+
+    def __str__(self):
+        """Unicode representation of Sales."""
+        return f"{self.purchase_id} - {str(self.total_amount)}"
---
+++ Sales/signals.py
@@ -0,0 +1,60 @@
+from django.db.models.signals import pre_delete,m2m_changed,post_save
+from django.dispatch import receiver
+from Branch.models import Branch_Product, Branch_Suit, Branch_Tops, Product_Size, Suit_Size, Tops_Size
+from Sales.models import Items, Sales
+
+
+def manage_stock(instance,branch,action='add'):
+    mapper = {'product':[Branch_Product,Product_Size],
+              'suit':[Branch_Suit,Suit_Size],
+              'top':[Branch_Tops,Tops_Size],
+              }
+    
+    model = instance.p_group
+    
+    if model == 'product':
+        branch_product,created = mapper[model][0].objects.get_or_create(branch = branch,
+                                                                    product = instance.product)
+        product_size,created_ = mapper[model][1].objects.get_or_create(branch_product = branch_product,
+                                                                        size = instance.size_instance) 
+    elif model == 'suit':
+        branch_product,created = mapper[model][0].objects.get_or_create(branch = branch,
+                                                                    product = instance.suit)
+        product_size,created_ = mapper[model][1].objects.get_or_create(branch_suit = branch_product,
+                                                                        size = instance.size_instance)
+    elif model == "top":
+        branch_product,created = mapper[model][0].objects.get_or_create(branch = branch,
+                                                                    product = instance.top)
+        product_size,created_ = mapper[model][1].objects.get_or_create(branch_tops = branch_product,
+                                                                        size = instance.size_instance)
+    
+    if action == 'delete' or action == 'remove' :
+        product_size.current_qty += instance.qty
+        product_size.save()
+        return
+ 
+    product_size.current_qty -= instance.qty
+    product_size.save()
+
+
+@receiver(m2m_changed, sender= Sales.items.through)
+def sales_multipleSIzes_changes(sender, instance,action,reverse,model,pk_set,**kwargs):
+    if action == "post_add" and instance.paid and instance.channel == "store":
+        for item_id in pk_set:
+            item = Items.objects.get(pk=item_id)
+            manage_stock(item,instance.branch,action='add')
+    elif action == 'pre_remove' and instance.paid:
+        for item_id in pk_set:
+            item = Items.objects.get(pk=item_id)
+            manage_stock(item,instance.branch,action='remove')
+            
+@receiver(post_save, sender=Sales)
+def mark_as_paid(sender, instance, *args, **kwargs): 
+    if instance.paid and instance.channel == "web":
+        for item in instance.items.all():
+            manage_stock(item,instance.branch,action='add')
+        
+@receiver(pre_delete, sender=Sales)
+def delete_sale(sender, instance, *args, **kwargs):
+    for item in instance.items.all():
+        manage_stock(item,instance.branch,action='delete')
