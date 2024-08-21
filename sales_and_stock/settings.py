cd2db7e7149550d505f9735d894e90a70e4eaac9
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2024-08-21 06:41:10 +0100

    updated allowed host

--- sales_and_stock/settings.py
+++ sales_and_stock/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://smbclassicwears.com.ng','www.smbclassicwears.com.ng/']
+ALLOWED_HOSTS = ['smbclassicwears.com.ng']
 
 
 # Application definition
